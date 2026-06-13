614ec5c3c0b9d68a432ffd0926d8a057a0b8c392
avi-nz <avishai@narsey.com>
2026-06-13 22:32:28 +1200

    added labelling for neutral games (NBA Cup games)

--- data_loader.py
+++ data_loader.py
@@ -12,31 +12,61 @@ def load_regular_season_games(season):
     games_df = df.sort_values("GAME_DATE")
 
     grouped_games = []
-    bad_games = []
+    neutral_games = []
 
     for game_id, game in games_df.groupby("GAME_ID"):
 
         home = game[game["MATCHUP"].str.contains("vs.", regex=False)]
         away = game[game["MATCHUP"].str.contains("@", regex=False)]
 
-        if len(home) != 1 or len(away) != 1:
-            bad_games.append(game_id)
-            continue
+        # Case 1: normal home/away game
+        if len(home) == 1 and len(away) == 1:
 
-        home = home.iloc[0]
-        away = away.iloc[0]
+            home = home.iloc[0]
+            away = away.iloc[0]
 
-        grouped_games.append({
-            "GAME_ID": game_id,
-            "DATE": home["GAME_DATE"],
+            grouped_games.append({
+                "GAME_ID": game_id,
+                "DATE": home["GAME_DATE"],
 
-            "HOME_TEAM": home["TEAM_NAME"],
-            "AWAY_TEAM": away["TEAM_NAME"],
+                "HOME_TEAM": home["TEAM_NAME"],
+                "AWAY_TEAM": away["TEAM_NAME"],
 
-            "HOME_PTS": home["PTS"],
-            "AWAY_PTS": away["PTS"]
-        })
+                "HOME_PTS": home["PTS"],
+                "AWAY_PTS": away["PTS"],
 
-    print(f"Bad games found: {len(bad_games)}")
+                "SITE_TYPE": "HOME_AWAY"
+            })
+
+        # Case 2: neutral site game (NBA Cup / special games)
+        # len of away = 2 cause both teams are marked as away when there is a neutral site.
+        elif len(home) == 0 and len(away) == 2:
+
+            # in neutral games, BOTH rows usually have "@"
+            # so we just take both teams safely
+            team_a = game.iloc[0]
+            team_b = game.iloc[1]
+
+            neutral_games.append(game_id)
+
+            grouped_games.append({
+                "GAME_ID": game_id,
+                "DATE": team_a["GAME_DATE"],
+
+                "HOME_TEAM": "TEAM_A",
+                "AWAY_TEAM": "TEAM_B",
+
+                "HOME_PTS": team_a["PTS"],
+                "AWAY_PTS": team_b["PTS"],
+
+                "SITE_TYPE": "NEUTRAL"
+            })
+
+        # unexpected structure - log it
+        else:
+            print(f"Unexpected game structure: {game_id}")
+            print(game[["TEAM_NAME", "MATCHUP"]])
+
+    print(f"Neutral games found: {len(neutral_games)}")
 
     return pd.DataFrame(grouped_games)
